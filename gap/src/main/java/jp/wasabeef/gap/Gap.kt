821dd4aa3700f076df5020c28927566523f50963
wasabeef <dadadada.chop@gmail.com>
2021-10-13 18:27:26 +0900

    feature: add Modifier param

--- gap/src/main/java/jp/wasabeef/gap/Gap.kt
+++ gap/src/main/java/jp/wasabeef/gap/Gap.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 
 @Composable
-fun RowScope.Gap(width: Dp) {
-  Spacer(Modifier.width(width))
+fun RowScope.Gap(width: Dp, modifier: Modifier = Modifier) {
+  Spacer(modifier = modifier.width(width))
 }
 
 @Composable
-fun ColumnScope.Gap(height: Dp) {
-  Spacer(Modifier.height(height))
+fun ColumnScope.Gap(height: Dp, modifier: Modifier = Modifier) {
+  Spacer(modifier = modifier.height(height))
+}
+
+@Composable
+fun LazyItemScope.Gap(padding: Dp, modifier: Modifier = Modifier) {
+  Spacer(modifier = Modifier.size(padding))
 }
--- sample/src/main/java/jp/wasabeef/gap/sample/MainActivity.kt
+++ sample/src/main/java/jp/wasabeef/gap/sample/MainActivity.kt
@@ -3,10 +3,20 @@ package jp.wasabeef.gap.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import jp.wasabeef.gap.Gap
 
@@ -14,9 +24,15 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Column() {
+      Column {
         ColumnSample()
+        Divider(modifier = Modifier.padding(12.dp))
         RowSample()
+        Divider(modifier = Modifier.padding(12.dp))
+        LazyColumnSample()
+        Divider(modifier = Modifier.padding(12.dp))
+        LazyRowSample()
+        Divider(modifier = Modifier.padding(12.dp))
       }
     }
   }
@@ -24,18 +40,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ColumnSample() {
-  Column {
-    Text(text = "Hello")
-    Gap(height = 20.dp)
-    Text(text = "World")
+  Column(modifier = Modifier.width(IntrinsicSize.Min)) {
+    Text(text = "Jetpack")
+    Gap(20.dp, modifier = Modifier
+      .fillMaxWidth()
+      .background(Color.Red))
+    Text(text = "Compose")
+    Gap(20.dp)
+    Text(text = "Gap")
   }
 }
 
 @Composable
 fun RowSample() {
-  Row {
-    Text(text = "Hello")
-    Gap(width = 20.dp)
-    Text(text = "World")
+  Row(modifier = Modifier.width(IntrinsicSize.Min)) {
+    Text(text = "Jetpack")
+    Gap(20.dp)
+    Text(text = "Compose")
+    Gap(20.dp)
+    Text(text = "Gap")
+  }
+}
+
+@Composable
+fun LazyColumnSample() {
+  LazyColumn(modifier = Modifier
+    .background(Color.LightGray)
+    .fillMaxWidth()) {
+    item {
+      Text(text = "First item1")
+      Gap(20.dp)
+      Text(text = "First item2")
+    }
+
+    items(3) { index ->
+      Text(text = "Item: $index")
+    }
+
+    item {
+      Text(text = "Last item1")
+      Gap(20.dp)
+      Text(text = "Last item2")
+    }
+  }
+}
+
+@Composable
+fun LazyRowSample() {
+  LazyRow {
+    item {
+      Text(text = "First item1")
+      Gap(20.dp)
+      Text(text = "First item2")
+    }
+
+    items(3) { index ->
+      Text(text = "Item: $index")
+    }
+
+    item {
+      Text(text = "Last item1")
+      Gap(20.dp)
+      Text(text = "Last item2")
+    }
   }
 }
